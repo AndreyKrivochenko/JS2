7fdf1a437ea480a4745b4acea7b49e4f4f2ddc6a
AndreyKrivochenko <krivochenko_andrey@mail.ru>
2021-09-16 09:49:05 +0700

    lesson1 completed

--- js/main.js
+++ js/main.js
@@ -1,13 +1,14 @@
 const products = [
-    {id: 1, title: 'Notebook', price: 2000},
-    {id: 2, title: 'Mouse', price: 20},
-    {id: 3, title: 'Keyboard', price: 200},
-    {id: 4, title: 'Gamepad', price: 50},
+    {id: 1, title: 'Notebook', price: 2000, img: 'img/no-img.png'},
+    {id: 2, title: 'Mouse', price: 20, img: 'img/no-img.png'},
+    {id: 3, title: 'Keyboard', price: 200, img: 'img/no-img.png'},
+    {id: 4, title: 'Gamepad', price: 50, img: 'img/no-img.png'},
 ];
 //Функция для формирования верстки каждого товара
 //Добавить в выводе изображение
 const renderProduct = (item) => {
     return `<div class="product-item">
+                <img src="${item.img}" alt="image">
                 <h3>${item.title}</h3>
                 <p>${item.price}</p>
                 <button class="buy-btn">Купить</button>
@@ -15,7 +16,6 @@ const renderProduct = (item) => {
 };
 const renderPage = list => {
     const productsList = list.map(item => renderProduct(item));
-    // console.log(productsList);
     let innerText = '';
     productsList.forEach(element => innerText += element);
     document.querySelector('.products').innerHTML = innerText;
